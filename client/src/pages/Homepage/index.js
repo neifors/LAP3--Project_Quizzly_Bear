f448b4ea46232165de49a44e08ad9c115a85003b
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2022-04-16 18:16:17 +0100

    homepage started

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './style.css'
 
-const Homepage = () => (
-    <h1>This is the page for the Homepage!</h1>
-)
+const Homepage = () => {
+    const goTo = useNavigate();
+
+    return (
+    <section>
+    <img src='./images/QUIZZLY BEARS no background.png' alt='Quizzly Bears Logo'></img>
+    <button onClick={() => {goTo('/login')}}> LOGIN </button>
+    <button onClick={() => {goTo('/login')}}> REGISTER </button>
+    </section>
+    )
+}
 
 export default Homepage;

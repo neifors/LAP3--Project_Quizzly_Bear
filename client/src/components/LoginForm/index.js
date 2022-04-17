fc51e8ebc58692ab6f114a06aed073399049e24d
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2022-04-17 11:00:28 +0100

    beginning testing and components for login

---
+++ client/src/components/LoginForm/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+function LoginForm() {
+
+    const [ username, setUsername ] = useState();
+    const [ password, setPassword ] = useState();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e)
+    }
+
+    const updateUsername = e => {
+        const input = e.target.value;
+        setUsername(input)
+    }
+
+    const updatePassword = e => {
+        const input = e.target.value;
+        setPassword(input)
+    }
+
+    return (
+        <form aria-label='form' onSubmit={handleSubmit}>
+            <input aria-label="Username" type='text' onChange={updateUsername} />
+            <input aria-label='Password' type='password' onChange={updatePassword} />
+            <input type='submit' value='Login' />
+            </form>
+    );
+};
+
+export default LoginForm;
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const Login = () => (
+const Login = () => {
+    
+    return (
     <h1>This is the page for the Login!</h1>
-)
+    )
+}
 
 export default Login;

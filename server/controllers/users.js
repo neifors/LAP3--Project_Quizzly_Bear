e8afc28569474a3f3f529917107290d3ee5f23af
isabel <isargp@gmail.com>
2022-04-17 10:25:23 +0100

    endpoint to get user by id WORKING

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,19 +5,38 @@ const bcrypt = require('bcryptjs');
 const User = require("../models/User")
 
 router.get('/', async (req, res) => {
+// Return a list of all users information apart from the password
 
   User.find({}, ['username', 'games'], (err, users) => {
     // Note that this error doesn't mean nothing was found,
     // it means the database had an error while searching, hence the 500 status
     if (err) return res.status(500).send(err)
-    // send the list of all users
     return res.status(200).send(users);
   });
 
 })
 
+router.get('/username/:username', async (req, res) => {
+// Return a user info by username (no password)
+
+  User.find({username: req.params.username}, ['username', 'games'], (err, users) => {
+    if (err) return res.status(500).send(err)
+    return res.status(200).send(users);
+  });
+})
+
+router.get('/id/:id', (req, res) => {
+// Return a user info by id (no password)
+  User.findById(req.params.id, ['username', 'games'],  (err, user) => {
+
+    if (err) return res.status(500).send(err)
+    return res.status(200).send(user)
+});
+})
+
 router.post('/register', async (req, res) => {
-  
+// Create a new user
+
   // Proccess to hash the password using bcryptjs 
   const salt = await bcrypt.genSalt();
   const hashed = await bcrypt.hash(req.body.password, salt)

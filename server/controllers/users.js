9283dfd272ca4891b141ce5eebcd6c9af9133eba
isabel <isargp@gmail.com>
2022-04-17 09:23:14 +0100

    get users added to endpoints

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,22 +4,36 @@ const bcrypt = require('bcryptjs');
 
 const User = require("../models/User")
 
+router.get('/', async (req, res) => {
 
+  User.find({}, ['username', 'games'], (err, users) => {
+    // Note that this error doesn't mean nothing was found,
+    // it means the database had an error while searching, hence the 500 status
+    if (err) return res.status(500).send(err)
+    // send the list of all users
+    return res.status(200).send(users);
+});
+
+})
 
 router.post('/register', async (req, res) => {
   
+  // Proccess to hash the password using bcryptjs 
   const salt = await bcrypt.genSalt();
   const hashed = await bcrypt.hash(req.body.password, salt)
 
+  // Object User with the username, the hashed password and an empty lists of games.
   const user = new User({
   username: req.body.username,
   password: hashed ,
   games: []
   });
 
+  // This is the way we save the object above into de database returning the new user if there is no error
   user.save((err, saveUser) => {
-    console.log(JSON.stringify(saveUser));
-  res.status(201).send("created")
+    if (err) return res.status(500).send(err);
+    console.log(saveUser)
+    return res.status(201).send(saveUser);
   });
 
 })
--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,9 @@
-const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User', new mongoose.Schema({
   username: String,
   password: String ,
-  games: [{
-      points: Number,
-      coins: Number,
-      opponent: ObjectId,
-      winner: Boolean
-  }]
+  games: []
 }));
 
 module.exports = User;

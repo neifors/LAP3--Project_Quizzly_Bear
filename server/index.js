905f3b9391e4c2528361c4fe0533068af10f8c19
isabel <isargp@gmail.com>
2022-04-18 13:38:29 +0100

    first socketio config added to server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,68 @@
 const mongoose = require('mongoose');
 const env = require('dotenv');
 env.config()
-const server = require('./server.js')
+const app = require('./server.js')
+const axios = require('axios')
 const port = process.env.PORT || 3000;
 
-// require("./db_config/dbconfig")();
+// const server = require("http").Server(app);
 
-server.listen(port, ()=>{
-    console.log(`server listening on port: ${port}`)
+// socket.io config
+const server = app.listen(port, ()=>{
+  console.log(`server listening on port: ${port}`)
 })
 
+const io = require("socket.io")(server, {
+  cors: {
+      origin: '*',
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+  }
+})
+
+
+io.on("connection", socket => {
+   console.log(socket.id)
+
+   // all socket.on()
+
+  socket.on("join-lobby", room => {
+    socket.join(room)
+    console.log(`User is in room: ${room}`)
+  })
+
+  socket.on("create-game", data => {
+
+    const callTriviaApi = async () => {
+      try {
+
+        const response = await axios.get(`https://opentdb.com/api.php?amount=${data.amount}&category=${data.category}&difficulty=${data.difficulty}&type=${data.type}`);
+        result = (response.data.results)
+      }
+      catch (err) {
+          console.log(err)
+      }
+      io.in(data).emit('questions-set', result)
+    }
+
+    callTriviaApi()
+
+  })
+})
+
+
+
+
+// DB CONNECTION
 mongoose.connect("mongodb://"+process.env.COSMOSDB_HOST+":"+process.env.COSMOSDB_PORT+"/"+process.env.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb", {
-   auth: {
-     username: process.env.COSMOSDB_USER,
-     password: process.env.COSMOSDB_PASSWORD
-   },
- useNewUrlParser: true,
- useUnifiedTopology: true,
- retryWrites: false
- })
- .then(() => console.log('Connection to CosmosDB successful'))
- .catch((err) => console.error(err));
+    auth: {
+      username: process.env.COSMOSDB_USER,
+      password: process.env.COSMOSDB_PASSWORD
+    },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  retryWrites: false
+  })
+  .then(() => console.log('Connection to CosmosDB successful'))
+  .catch((err) => console.error(err));
--- server/server.js
+++ server/server.js
@@ -1,27 +1,41 @@
 const express=require('express');
 const cors= require('cors');
-const server=express();
-server.use(cors("*"));
-server.use(express.json());
+const app=express();
+app.use(cors("*"));
+app.use(express.json());
 const path = require('path');
 
 
 const userRoutes = require("./controllers/users");
 const gameRoutes = require("./controllers/games");
 
-server.use("/users", userRoutes);
-server.use("/games", gameRoutes);
+app.use("/users", userRoutes);
+app.use("/games", gameRoutes);
 
 const htmlIndexRoute = path.join(__dirname, '/src/template/index.html')
 const cssRoute = path.join(__dirname, '/src/template/index.css')
 
-server.get('/', (req, res)=>{
+app.get('/', (req, res)=>{
     res.sendFile(htmlIndexRoute)
 })
-server.get('/styles.css', (req, res) => {
+app.get('/styles.css', (req, res) => {
     res.sendFile(cssRoute);
   });
 
+// const server = require("http").Server(app);
 
-module.exports= server;
+// const io = require("socket.io")(server)
+
+// io.on("connection", socket => {
+//    console.log(socket.id)
+
+   // all socket.on()
+
+   // socket.on("join-lobby", room => {
+   //    socket.join(room)
+   // })
+
+// })
+
+module.exports= app
 

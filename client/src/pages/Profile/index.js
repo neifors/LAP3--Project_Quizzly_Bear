9ec95c25e7bff5edc7f9e6dafc8b188fc3d1a74b
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2022-04-20 13:43:10 +0100

    adding score to profile

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import jwt from 'jwt-decode'
+import { getLeaderboardData } from '../../actions';
 import { LeaderboardButton, Logout, StartGame } from '../../components';
 import './style.css'
 import DeleteButton from '../../components/DeleteButton';
@@ -7,11 +8,21 @@ import HomeButton from '../../components/HomeButton';
 
 const Profile = () => {
 
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        async function getData () {
+            const data = await getLeaderboardData();
+            setData(data)
+        }
+        getData()
+    }, [])
+
     const userInfo = localStorage.getItem('token')
     const username = jwt(userInfo).username
     const dislpayName = username.charAt(0).toUpperCase() + username.slice(1);
-    const score = 'freaking loads'
-
+    const userData = data && data.find(x => x.username == `${username}`)
+    const score = userData && userData.score
     return (
         <section className='profile'>
             <img src='https://drive.google.com/uc?export=view&id=1or22FAHZtztxmNLSqAlZetPPLhsrX702' alt='Icon' />

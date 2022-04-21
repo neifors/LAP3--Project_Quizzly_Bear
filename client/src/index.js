a63500588baf06b1d6c6bd7ccd6033b1e5689874
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-04-21 10:02:09 +0100

    clean index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
-
-//ReactDOM.render(
-//    <React.StrictMode>
-//    <Router>
-//        <Provider store={store}>
-//        <App />
-//        </Provider>
-//    </Router>
-//    </React.StrictMode>,
-//document.getElementById("root"));
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);

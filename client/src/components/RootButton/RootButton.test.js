09256e3d26ccf6c6855b9334f5f35e1df1c9b433
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2022-04-21 15:46:10 +0100

    changing files for testing

--- client/src/components/RootButton/RootButton.test.js
+++ client/src/components/RootButton/RootButton.test.js
@@ -1,14 +1,14 @@
-import { default as HomeButton } from '.';
+import { default as RootButton } from '.';
 import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
-describe('HomeButton', () => {
+describe('RootButton', () => {
     beforeEach(() => {
-        render(<HomeButton />, { wrapper: MemoryRouter });
+        render(<RootButton />, { wrapper: MemoryRouter });
     })
 
     test('renders a button', () => {
         const btn = screen.getByRole('button')
-        expect(btn.textContent).toContain('Back to Homepage');
+        expect(btn.textContent).toContain('Back To Start');
     })
 })
--- client/src/pages/Game/Game.test.js
+++ client/src/pages/Game/Game.test.js
@@ -1,16 +1,32 @@
 import { default as Game } from '.';
+import { Timer } from '.';
 import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Game', () => {
 
-beforeEach(() => {
-    render(<Game/>, { wrapper: MemoryRouter });
-})
+    beforeEach(() => {
+        const div = document.createElement('div');
+        div.id = 'root'
+        window.domNode = div;
+        document.body.appendChild(div);
+        render(<Game />, { wrapper: MemoryRouter });
+    })
 
-test('it renders heading', ()=> {
-    const paragraph = screen.getByRole('heading');
-    expect(paragraph.textContent).toContain('Welcome to Quizzly Bears🐻')
-})
+    test('it renders heading', () => {
+        const paragraph = screen.getByRole('heading');
+        expect(paragraph.textContent).toContain(`Welcome to Quizzly Bears' quiz 🐻`)
+    })
+
+    test('it renders a form', () => {
+        const form = screen.getByRole('form')
+        expect(form).toBeInTheDocument
+    })
+
+    test('Timer renders a span', () => {
+        render(Timer)
+        const h3 = screen.getByRole('heading')
+        expect(h3).toBeInTheDocument
+    })
 
 })

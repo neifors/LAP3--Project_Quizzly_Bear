64c61a4ec9c4a6a3c0050e04005d66c62278eb05
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-04-21 10:01:54 +0100

    delete references to redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 //import ReactDOM from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App'
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from './store';
 
 //ReactDOM.render(
 //    <React.StrictMode>

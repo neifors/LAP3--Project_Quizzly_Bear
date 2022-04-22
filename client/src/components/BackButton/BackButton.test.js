ce0b2486cfc4b0eebfb496ba81b5c9993aa60e6d
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2022-04-22 11:38:39 +0100

    increasing coverage for backbutton

--- client/src/components/BackButton/BackButton.test.js
+++ client/src/components/BackButton/BackButton.test.js
@@ -2,6 +2,15 @@ import { default as BackButton } from '.';
 import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+  
+    return {
+      __esModule: true,
+      ...originalModule,
+      useNavigate: jest.fn(),
+    };
+  });
 
 describe('BackButton', () => {
     beforeEach(() => {
--- client/src/components/BackButton/index.js
+++ client/src/components/BackButton/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+  
+    return {
+      __esModule: true,
+      ...originalModule,
+      useNavigate: jest.fn(),
+    };
+  });
+
 const BackButton = () => {
 
     const goTo = useNavigate();

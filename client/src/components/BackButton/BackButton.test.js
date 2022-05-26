377eae292895f392b8fc661c5bd6e0a415c51887
isabel <isargp@gmail.com>
2022-05-26 11:51:49 +0100

    before deploying

--- client/src/components/BackButton/BackButton.test.js
+++ client/src/components/BackButton/BackButton.test.js
@@ -1,7 +1,16 @@
 import { default as BackButton } from '.';
 import { screen, render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, useNavigate } from 'react-router-dom';
 
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+  
+    return {
+      __esModule: true,
+      ...originalModule,
+      useNavigate: jest.fn(),
+    };
+});
 
 describe('BackButton', () => {
     beforeEach(() => {
@@ -11,6 +20,8 @@ describe('BackButton', () => {
     test('renders a button', () => {
         const btn = screen.getByRole('button')
         expect(btn.textContent).toContain('Go Back');
+        userEvent.click(btn)
+        expect(useNavigate).toHaveBeenCalled()
     })
 
 })
